dd5cc1dbe6a41908138839e26273c9a6ba5f122e
Irvin Reyes <reyes.yerp@gmail.com>
2019-01-12 00:06:53 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,109 @@
+class TicTacToe
+  WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(input)
+    input.to_i - 1
+  end
+
+  def move(index, character = "X")
+    @board[index] = character
+  end
+
+  def position_taken?(index)
+    !(@board[index].nil? || @board[index] == " ")
+  end
+
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
+    else
+      turn
+    end
+  end
+
+  def turn_count
+    counter = 0
+    @board.each do |box|
+      if !(box == "" || box == " " || box == nil)
+        counter += 1
+      end
+    end
+    counter
+  end
+
+  def current_player
+    turn_count.even? ? "X" : "O"
+  end
+
+  def won?
+    WIN_COMBINATIONS.each do |win_combination|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+
+      position_1 = @board[win_index_1]
+      position_2 = @board[win_index_2]
+      position_3 = @board[win_index_3]
+
+      if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+        return win_combination
+      end
+    end
+    false
+  end
+
+  def full?
+    @board.all? do |box|
+      !(box.nil? || box == " ")
+    end
+  end
+
+  def draw?
+    full? && !won?
+  end
+
+  def over?
+    full? || won? || draw?
+  end
+
+  def winner
+    combination = won?
+    combination ? @board[combination.first] : nil
+  end
+
+  def play
+    until over?
+      turn
+    end
+    won? ? puts("Congratulations #{winner}!") : puts("Cat's Game!")
+  end
+end
